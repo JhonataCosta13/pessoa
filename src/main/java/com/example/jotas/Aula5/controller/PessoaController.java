a384c6b0921edcbad757782a42ad93b32866efa1
Jhonata Costa <costajhonata8@gmail.com>
2024-01-03 21:26:05 -0300

    get

--- src/main/java/com/example/jotas/Aula5/controller/PessoaController.java
+++ src/main/java/com/example/jotas/Aula5/controller/PessoaController.java
@@ -1,14 +1,43 @@
 package com.example.jotas.Aula5.controller;
 
+import com.example.jotas.Aula5.model.Pessoa;
 import com.example.jotas.Aula5.repository.PessoaRepository;
+import com.example.jotas.Aula5.service.PessoaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("pessoa")
 public class PessoaController {
 
     @Autowired
-    private PessoaRepository pessoaRepository;
+    private PessoaService pessoaService;
+
+    @GetMapping
+    public ResponseEntity<List<Pessoa>> buscarPessoas(){
+
+        List<Pessoa> pessoas = pessoaService.getPessoas();
+
+        return ResponseEntity.ok().body(pessoas);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> criarPessoa(Pessoa pessoaInput){
+
+        Pessoa pessoaOutput = pessoaService.savePessoa(pessoaInput);
+        URI location = UriComponentsBuilder.
+                fromUriString("http://localhost:8080/pessoa/{id}").
+                buildAndExpand(pessoaOutput.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+
+    }
 }
--- src/main/java/com/example/jotas/Aula5/service/PessoaService.java
+++ src/main/java/com/example/jotas/Aula5/service/PessoaService.java
@@ -1,11 +1,23 @@
 package com.example.jotas.Aula5.service;
 
+import com.example.jotas.Aula5.model.Pessoa;
+import com.example.jotas.Aula5.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PessoaService {
 
     @Autowired
-    private PessoaService pessoaService;
+    private PessoaRepository pessoaRepository;
+
+    public Pessoa savePessoa(Pessoa pessoaInput) {
+        return pessoaRepository.save(pessoaInput);
+    }
+
+    public List<Pessoa> getPessoas() {
+        return pessoaRepository.findAll();
+    }
 }
